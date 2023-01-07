d3339a818aa547172549fbda28d7409b4e2f8bb4
samettoprak <topraksamet41@gmail.com>
2023-01-07 15:47:33 +0300

    Initial commit

---
+++ src/main/java/com/samettoprak/WhatsAppwithRestAPI/WhatsAppWithRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.samettoprak.WhatsAppwithRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppWithRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppWithRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samettoprak/WhatsAppwithRestAPI/WhatsAppWithRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samettoprak.WhatsAppwithRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppWithRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
